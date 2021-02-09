6842ad9d33e9602878b1fee04611e32c8f459879
Daniele <danlup94@gmail.com>
2021-02-09 16:10:57 +0100

    Esercizio base completato

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,1 +1,20 @@
-console.log('hello');
+// Inizializzazione Vue
+var app = new Vue ({
+  el: '#app',
+  data:{
+    album: []  // Array degli album
+  },
+
+// Funzione chiamata axios
+  mounted: function () {
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(risposta => {
+
+      this.album = risposta.data.response;
+    });
+
+    console.log(this.album);
+  }
+
+});
